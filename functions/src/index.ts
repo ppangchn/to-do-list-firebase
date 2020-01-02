23adee57ec0d5b8a31beb34d99474ad3eeb9d3fb
Chanissa Trithipkaiwanpon <pang.chanissa@gmail.com>
2020-01-02 11:03:56 +0700

    build

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,10 @@
-import * as functions from 'firebase-functions';
 import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import { makeSchema } from 'nexus';
 import { Query } from './types/Query';
 import { Mutation } from './types/Mutation';
+
+const functions = require('firebase-functions');
 
 const schema = makeSchema({
 	types: [Query, Mutation],
